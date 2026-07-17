f2da8c84128496b4ff1be57fb2de8f4035b13ee5
vip23anchib <barman23vipanchi@gmail.com>
2026-07-18 01:45:42 +0530

    Time: 0 ms (100%), Space: 12.6 MB (40.34%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.cpp
+++ 0162-find-peak-element/0162-find-peak-element.cpp
@@ -11,11 +11,18 @@ public:
         if(nums[n-1]>nums[n-2]){
             return n-1;
         }
-        for(int i=1;i<n-1;i++){
-            if (nums[i]>nums[i-1]&&nums[i]>nums[i+1]){
-                return i;
+        int low=1;
+        int high=n-1;
+
+        while(low<high){
+            int mid=(low+high)/2;
+            if(nums[mid]<nums[mid+1]){
+                low=mid+1;
+            }
+            else{
+                high=mid;
             }
         }
-    return -1;  
+    return low;  
     }
 };
