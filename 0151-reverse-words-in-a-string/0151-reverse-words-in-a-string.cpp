d970f052668636b2ca527a6f6dbebc6e8d24721b
vip23anchib <barman23vipanchi@gmail.com>
2026-07-29 16:04:40 +0530

    Time: 3 ms (34.12%), Space: 11.2 MB (21.72%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
@@ -6,34 +6,32 @@ public:
         int n = s.size();
         int i = 0;
 
-        while(i < n){
-
-            while(i < n && s[i] == ' ')
-                i++;
-
-            string word = "";
-
-            while(i < n && s[i] != ' '){
-                word += s[i];
+        while(i<n){
+            while(i<n && s[i]==' '){
                 i++;
             }
+            string word="";
 
-            if(!word.empty())
+            while(i<n && s[i]!=' '){
+                word+=s[i];
+                i++;
+            }
+            if (!word.empty()){
                 words.push_back(word);
+            }
+
         }
+        
 
-        reverse(words.begin(), words.end());
+        reverse(words.begin(),words.end());
+        string ans="";
+        for(i=0;i<words.size();i++){
+            ans+=words[i];
+            if(i!=words.size()-1){
+                ans+=" ";
+            }
 
-        string ans = "";
-
-        for(int i = 0; i < words.size(); i++){
-
-            ans += words[i];
-
-            if(i != words.size()-1)
-                ans += " ";
-        }
-
-        return ans;
-    }
+        }  
+    return ans;      
+   }
 };
