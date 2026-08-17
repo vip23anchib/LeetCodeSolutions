5294c1899f5a9108bfd765eeca51bdfaf8833689
vip23anchib <barman23vipanchi@gmail.com>
2026-08-17 23:51:08 +0530

    Time: 0 ms (100%), Space: 9.2 MB (52.28%) - LeetHub

---
+++ 0389-find-the-difference/0389-find-the-difference.cpp
@@ -0,0 +1,24 @@
+
+class Solution {
+public:
+    char findTheDifference(string s, string t) {
+
+        int n = s.size();
+
+        sort(s.begin(), s.end());
+        sort(t.begin(), t.end());
+
+        for(int i = 0; i < n; i++) {
+
+            if(s[i] == t[i]) {
+                continue;
+            }
+            else {
+                return t[i];
+            }
+        }
+
+
+        return t[n];
+    }
+};
