45419ec1c3cc637a59580c44241b1b741c84764b
vip23anchib <barman23vipanchi@gmail.com>
2026-08-19 16:07:22 +0530

    Time: 0 ms (100%), Space: 28.5 MB (40.01%) - LeetHub

--- 0503-next-greater-element-ii/0503-next-greater-element-ii.cpp
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.cpp
@@ -3,21 +3,19 @@ public:
     vector<int> nextGreaterElements(vector<int>& nums) {
         
         int n=nums.size();
-        vector<int>ans;
+        vector<int>ans(n);
+
+        stack<int>st;
+
+        for(int i=2*n-1;i>=0;i--){
+            while(!st.empty() && st.top()<=nums[i%n]){
+                st.pop();
+            }
+            if(i<n){
+                ans[i]=st.empty()? -1 : st.top();
+            }
+            st.push(nums[i%n]);      
         
-        for(int i=0;i<n;i++){
-            bool found=false;
-            for(int j=i+1;j<i+n;j++){
-                int ind=j%n;
-                if(nums[ind]>nums[i]){
-                    ans.push_back(nums[ind]);
-                    found=true;
-                    break;
-                }
-            }
-            if(found==false){
-                ans.push_back(-1);
-            }
         }
     return ans;
     }
