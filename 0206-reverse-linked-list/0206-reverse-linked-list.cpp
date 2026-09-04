5466f6d6bce0a4e1ae280c4c306c85298985515e
vip23anchib <barman23vipanchi@gmail.com>
2026-09-04 23:22:19 +0530

    Time: 0 ms (100%), Space: 13.5 MB (41.39%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.cpp
+++ 0206-reverse-linked-list/0206-reverse-linked-list.cpp
@@ -8,18 +8,20 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+ 
 class Solution {
 public:
     ListNode* reverseList(ListNode* head) {
-        ListNode* prev=NULL;
-        ListNode* curr=head;
 
-        while(curr!=NULL){
-            ListNode* front=curr->next;
-            curr->next=prev;
-            prev=curr;
-            curr=front;
-        }
-    return prev;   
+        if (head == NULL || head->next == NULL)
+            return head;
+
+        ListNode* newHead = reverseList(head->next);
+
+        head->next->next = head;
+
+        head->next = NULL;
+
+        return newHead;
     }
 };
