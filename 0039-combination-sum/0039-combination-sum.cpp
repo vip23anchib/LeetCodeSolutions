92b1251dc0ac56e4ca298ec017f784a8ee18b782
vip23anchib <barman23vipanchi@gmail.com>
2026-09-07 01:07:13 +0530

    Time: 0 ms (100%), Space: 14.1 MB (76.5%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.cpp
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -1,29 +1,24 @@
 class Solution {
 public:
 
-    void solve(vector<int>& candidates, int target,
-               int i, vector<int>& current,
-               vector<vector<int>>& ans) {
-
-        if (target == 0) {
-            ans.push_back(current);
+    void solve(vector<int>& candidates, int target, int index, vector<int>& current,vector<vector<int>>& ans) {
+        if(index==candidates.size()){
+            if(target==0){
+                ans.push_back(current);
+            }
             return;
         }
 
-        if (target < 0 || i == candidates.size()) {
-            return;
+        if(candidates[index]<=target){
+            current.push_back(candidates[index]);
+
+            solve(candidates,target-candidates[index],index,current,ans);
+
+            current.pop_back();
         }
 
-        current.push_back(candidates[i]);
-
-        solve(candidates, target - candidates[i],
-              i, current, ans);
-
-        current.pop_back();
-
-
-        solve(candidates, target,
-              i + 1, current, ans);
+        solve(candidates,target,index+1,current,ans);
+        
     }
 
 
