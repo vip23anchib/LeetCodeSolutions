232c06b2a49800b2cf86e1ba778cb8824c63e4e9
Vipanchi Barman <barman23vipanchi@gmail.com>
2026-09-18 00:00:00 +0530

    Time: 0 ms (100%), Space: 8.5 MB (86.98%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+
+        int low = 0;
+        int high = x;
+
+        int ans = 0;
+
+        while(low <= high) {
+
+            int mid = low + (high - low) / 2;
+
+            if(mid == 0 || mid <= x / mid) {
+                ans = mid;      
+
+                low = mid + 1;
+            }
+            else {
+                
+                high = mid - 1;
+            }
+        }
+
+        return ans;
+    }
+};
